c318388d976ea549c5f2450a942b85db29af25eb
hgb926 <hgb926@naver.com>
2024-04-26 23:12:23 +0900

    0426 장바구니, 물품빼기 구현

---
+++ src/kibeom/CartRepository.java
@@ -0,0 +1,86 @@
+package kibeom;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Objects;
+
+public class CartRepository {
+    // 장바구니 물품 관리
+
+    private Map<String, Map<String, Object>> item;
+
+
+    public CartRepository() {
+        item = new HashMap<>();
+        // 상품 초기화
+        addItem("crocs", "SHOES", 40000, "unisex");
+        addItem("t-shirt", "TOP", 20000, "male");
+        addItem("nikeAirForce", "SHOES", 80000, "unisex");
+        addItem("jean", "BOTTOM", 30000, "unisex");
+        addItem("riderJacket", "TOP", 90000, "male");
+        addItem("poloCap", "CAP", 60000, "unisex");
+    }
+
+    public void addItem(String itemName, String type, int price, String gender) {
+        Map<String, Object> itemDetails = new HashMap<>();
+        itemDetails.put("type", type);
+        itemDetails.put("price", price);
+        itemDetails.put("gender", gender);
+        item.put(itemName, itemDetails);
+    }
+
+    public void deleteItem(String itemName) {
+        if (item.containsKey(itemName)) {
+            item.remove(itemName);
+            System.out.println(itemName + " 항목이 삭제되었습니다.");
+        } else {
+            System.out.println(itemName + " 항목이 존재하지 않습니다.");
+        }
+    }
+
+
+
+    public Map<String, Map<String, Object>> getItems() {
+        return item;
+    }
+
+    public CartRepository(Map<String, Map<String, Object>> item) {
+        this.item = item;
+    }
+
+    public Map<String, Map<String, Object>> getItem() {
+        return item;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        CartRepository that = (CartRepository) o;
+        return Objects.equals(item, that.item);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(item);
+    }
+
+    @Override
+    public String toString() {
+        return "CartRepository{" +
+                "item=" + item +
+                '}';
+    }
+}
+/*
+        ========= 값에 접근하는 법 =========
+        public static void main(String[] args) {
+
+        CartRepository cartRepository = new CartRepository();
+
+        // 장바구니에 담긴 상품 출력
+        Map<String, Map<String, Object>> items = cartRepository.getItems();
+        for (String s : items.keySet()) {
+            System.out.println(items.get(s).get("price"));
+        }
+    }
+ */
