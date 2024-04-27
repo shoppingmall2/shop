7643a6f43c0194823a630c4cc047e5be77c92bda
hgb926 <hgb926@naver.com>
2024-04-27 14:32:47 +0900

    0427 test commit

---
+++ src/kibeom/Test.java
@@ -0,0 +1,21 @@
+package kibeom;
+
+import java.util.Map;
+
+public class Test {
+    public static void main(String[] args) {
+
+        CartRepository cartRepository = new CartRepository();
+        CartView.showCartRepository();
+
+
+
+    }
+}
+        // 장바구니에 담긴 상품 출력
+//        cartRepository.addItem("cargoPants", "BOTTOM", 15000, "famale");
+//        cartRepository.deleteItem("poloCap");
+//        Map<String, Map<String, Object>> items = cartRepository.getItems();
+//        for (String s : items.keySet()) {
+//            System.out.println(s);
+//        }
