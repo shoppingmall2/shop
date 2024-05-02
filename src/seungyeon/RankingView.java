f5daf65d76f711f479178d9c3b16ae1369dc74c9
seungyeonlee2 <tmdduslove@naver.com>
2024-05-02 15:27:14 +0900

    :bug: 파일 컨플릭트 수정

--- src/seungyeon/RankingView.java
+++ src/seungyeon/RankingView.java
@@ -1,6 +1,6 @@
 package seungyeon;
 
-import java.util.Map;
+import java.util.*;
 //
 //public class RankingView {
 //    public void displayTopRankings(Map<String, Integer> topRankings) {
@@ -15,16 +15,36 @@ import java.util.Map;
 //    }
 //}
 
-import java.util.List;
 
 public class RankingView {
     public void displayTopRankings(List<Item> topRankings) {
+        Set<Item> top10Item = new HashSet<>(topRankings);
+
+        List<Item> top10ItemList = new ArrayList<>(top10Item);
+
+        int size = Math.min(top10ItemList.size(), 10);
+
         System.out.println("Top 10 Rankings:");
-        int rank = 1;
-        for (Item item : topRankings) {
-            System.out.println(rank + ". " + item.getItemName() + " - Price: " + item.getPrice());
-            rank++;
+
+        Random random = new Random();
+
+        for (int i = 0; i < size; i++) {
+            // 아이템 랜덤 선택
+            int randomIndex = random.nextInt(top10ItemList.size());
+            Item randomItem = top10ItemList.get(randomIndex);
+            System.out.println((i + 1) + ". " + randomItem.getItemName() + " - Price: " + randomItem.getPrice());
+
+            top10ItemList.remove(randomIndex);
         }
-        System.out.print("\n");
+        System.out.println();
+
+//        int rank = 1;
+//        for (Item item : topRankings) {
+//            System.out.println(rank + ". " + item.getItemName() + " - Price: " + item.getPrice());
+//            rank++;
+//
+//        }
+
+//        System.out.print("\n");
     }
 }
