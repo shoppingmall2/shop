2b6e34ee6653e4084cea51b5a64e992325d8c576
hgb926 <hgb926@naver.com>
2024-05-02 14:19:00 +0900

    0502 주문시간 구현 전

--- src/kibeom/Cart.java
+++ src/kibeom/Cart.java
@@ -2,6 +2,7 @@ package kibeom;
 
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
+import java.util.Calendar;
 
 public class Cart {
 
@@ -22,6 +23,17 @@ public class Cart {
 
     }
 
+
+    public static String orderTime() {
+        Calendar currentTime = Calendar.getInstance();
+        int year = currentTime.get(Calendar.YEAR);
+        int month = currentTime.get(Calendar.MONTH) + 1; // 월은 0부터 시작하므로 +1
+        int day = currentTime.get(Calendar.DAY_OF_MONTH);
+        int hour = currentTime.get(Calendar.HOUR_OF_DAY);
+        int minute = currentTime.get(Calendar.MINUTE);
+        int second = currentTime.get(Calendar.SECOND);
+        return "주문 시간: " + year + "-" + month + "-" + day + " " + hour + ":" + minute + ":" + second;
+    }
 
 
     public Cart() {
